f33d76c430d070342e5bf309102be7ccabd22b11
jobaydulcse <jobaydul.cse@gmail.com>
2025-08-11 11:19:02 +0600

    goods file added

---
+++ good.js
@@ -0,0 +1,21 @@
+/*
+ *chair --> 3cft
+ *table --> 10 cft
+ *bed --->50cft
+ */
+
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
+  const perChairWood = 3;
+  const perTableWood = 10;
+  const perBedWood = 50;
+
+  const chairTotalWood = chairQuantity * perChairWood;
+  const tableTotalWood = tableQuantity * perTableWood;
+  const bedTotalWood = bedQuantity * perBedWood;
+
+  const totalWood = chairTotalWood + tableTotalWood + bedTotalWood;
+  return totalWood;
+}
+
+const wood = woodQuantity(6, 1, 3);
+console.log("wood need ", wood);
